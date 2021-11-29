3506ec3578a622bd8964f245bc45856bbe092a5a
rdy24 <raflizardeswandiy24@gmail.com>
2021-11-29 10:49:59 +0700

    git init dan push ke repository

---
+++ assets/script/script.js
@@ -0,0 +1,7 @@
+  
+const toggleButton = document.querySelector('.toggle-button')
+const menu = document.querySelector('.menu')
+
+toggleButton.addEventListener('click', () => {
+  menu.classList.toggle('active')
+})
